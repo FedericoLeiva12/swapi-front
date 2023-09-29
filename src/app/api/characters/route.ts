560b6146f2e29785f6954f5df0db79abd32cfbdd
InvBoy <49959270+FedericoLeiva12@users.noreply.github.com>
2023-09-29 17:15:39 +0000

    fix dynamic behaviour oncharacter route

--- src/app/api/characters/route.ts
+++ src/app/api/characters/route.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 import type { ICharacter } from './types';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   try {
     const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/characters?page=${request.nextUrl.searchParams.get('page') || 1}`)
